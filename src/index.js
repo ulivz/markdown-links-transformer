ba1653daa32caedfaae0da535764a1cc56274aee
ulivz <472590061@qq.com>
2018-02-09 00:49:41 +0800

    chore: ignore idea

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function inlineToSeperate(str) {
   let links = []
   str = str.replace(inlinelinkReg, (match, text, link) => {
     links.push(link)
-    return `${text}[${links.length}]`
+    return `${text}[${links.length + 1}]`
   })
   return str + '\n\n' + links.map((link, index) => `[${index}]: ${link}`).join('\n')
 }
